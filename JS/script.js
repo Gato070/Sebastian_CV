245b3a992e20eab375ecb114ea4f0cd1015daa3c
Sebastian Villegas <juliovillegas@hotmail.es>
2023-01-29 18:04:15 -0300

    Primera presentación CV

--- JS/script.js
+++ JS/script.js
@@ -1,115 +1,29 @@
 
+const api_url_1 = "https://randomuser.me/api/?results=10";
 
-window.onload = function getUser() {
-    const myObj = fetch(' https://randomuser.me/api/ ')
-        .then(response => response.json())
-        .then(json => console.log(json));
-    let text = "";
-    for (const x in myObj) {
-        text += myObj[x] + ", ";
-    }
-    document.getElementById("direccion").innerHTML = text;
-}
-/*
-    const text = '[ "Ford", "BMW", "Audi", "Fiat" ]';
-    const myArr = JSON.parse(text);
-    document.getElementById("demo").innerHTML = myArr[0];
-
-
-/*
-document.getElementById("direccion").innerHTML =` ${myObj.Array(1)}`;
-
-/*
-
-
-    const obj = JSON.parse(myObj);
-    console.log(obj);
+const htmlnombre = document.querySelector('#app');
+const htmlimagen = document.querySelector('#img');
 
 
 
+fetch('https://randomuser.me/api/?results=1')
+    .then(response => response.text())
+    .then(responseText => {
+        let users = JSON.parse(responseText).results;
+        console.log('Este es el objeto de usuarios', users);
 
-fetch(' https://randomuser.me/api/ ')
-     .then(response => response.json())
-     .then(json => console.log(json))   
+        const jsv = users.map
+            (results =>
+                `<h1>${results.name.title}
+                     ${results.name.first} 
+                     ${results.name.last}<br> 
+                   @ ${results.email}</h1>`)
 
+            htmlnombre.innerHTML = `<ul>${jsv}</ul>`
 
+        const img = users.map
+            (results => `${results.picture.medium}`)
 
-<script>
-     jQuery.ajax({
-         url: 'https://randomuser.me/api/',
-         dataType: 'json',
-         success: function (data) {
-             console.log(data);
-         }
-     });
+        htmlimagen.innerHTML = `<p>${img}</p>`
 
- </script>
-*/
-/*
- <script>
-     //leer pagina desarrollo web para entender esto
-
-     fetch('https://randomuser.me/api/?results=10')
-         .then(response => {
-             if (response.status == 200) {
-                 return response.text();
-             } else {
-                 throw "Respuesta incorrecta del servidor"
-             }
-         })
-         .then(responseText => {
-             let users = JSON.parse(responseText).results;
-             console.log('Este es el objeto de usuarios', users);
-         })
-         .catch(err => {
-             console.log(err);
-         });
- </script>
-
-*/
-
-/*REVISAR ESTA INFO ACA*/
-
-/*
-document.getElementById('boton').onclick 
-= function () {
-    console.log("Capturamos el evento clic");
-    document.getElementById("demo").innerHTML 
-    = "Estamos probando de cambiar el texto con javaScript";
-}
-
-document.addEventListener('click', function() {
-    console.log("Hola mundo");
-    document.getElementById("demo").innerHTML 
-    = "Estamos probando de cambiar el texto con javaScript";
-})
-*/
-/*
-document.getElementById('boton').addEventListener('click', function() {
-    console.log("Hola mundo");
-    document.getElementById("demo").innerHTML 
-    = "Estamos probando de cambiar el texto con javaScript";
-});
-
-document.getElementById('boton_fondo').addEventListener('click', function() {
-    document.body.style.backgroundColor = '#FF0000';
-    
-});
-
-document.getElementById('boton_default').addEventListener('click', function() {
-    document.body.style.backgroundColor = 'coral';
-    
-});
-
-document.getElementById('boton_ocultar').addEventListener('click', function() {
-    document.getElementById('demo').style.display = 'none';
-    
-});
-
-const collection = document.getElementsByClassName('parrafo');
-for (let i = 0; i < collection.length; i++) {
-    collection[i].style.backgroundColor= 'yellow';
-};
-
-*/
-
+    });
